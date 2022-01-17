eb2cff8d2c34064a32c6a299183cd64c7dba9cf6
KevinWeber <kevin.ninjabot@gmail.com>
2022-01-17 12:10:41 -0500

    New Repo, Week 7 Day 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+// api key:  dMHvzUBF
+
+
+
 
 function App() {
+  const apiKey = 'dMHvzUBF';
+  const [art, setArt] = useState([]);
+  const [userInput, SetUserInput] = useState();
+  const [searchTerm, setSearchTerm] = useState('');
+  
+
+  // Make API call to Rijks Museum API
+  useEffect(() => {
+    axios ({
+      url: 'https://www.rijksmuseum.nl/api/en/collection',
+      method: 'GET',
+      dataResponse: 'json',
+      params: {
+        key: apiKey,
+        imgonly: true,
+        q: searchTerm, 
+      }
+    }).then ((response) => {
+        console.log(response);
+        setArt(response.data.artObjects);
+    });
+  }, [searchTerm] );
+  
+const handleInput = (event) => {
+  console.log ('Is this working', event.target.value);
+  SetUserInput(event.target.value);
+  
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  setSearchTerm(userInput);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to the Mount Joey Deal With It National Gallery</h1>
+      <h2>🌽We present you with our most top cob art collection.🌽</h2>
+
+    <form onSubmit= { handleSubmit}>
+      <label htmlFor="search"> Search for art:</label>
+    </form>
+    <form>
+      <label htmlFor="search"> Search for art:</label>
+      <input type='text' id='search' onChange ={ handleInput } value ={userInput}/>
+      <button>Looky Looky!</button>
+    </form>
+
+    {art.map ((artwork) => {
+    return (
+      <div key ={artwork.id}>
+      <h2>{artwork.longTitle}</h2>
+      <img src ={artwork.webImage.url} alt={artwork.title} />
+      </div>
+    ) 
+  }
+  )}
+
     </div>
   );
 }
